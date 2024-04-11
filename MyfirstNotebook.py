f434075039099f5f8294e54d2a5c664cfbea7546
javi040898 <javi040898>
2024-04-11 14:58:37 +0000

    add new file

---
+++ MyfirstNotebook.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC # Header 1
+# MAGIC ## Header 2
+# MAGIC **bold text**
+# MAGIC *italics text*
+# MAGIC ~~strikethrough text~~
+# MAGIC `monospace text`
+# MAGIC ---
+# MAGIC > Block quote
+# MAGIC Ordered list:
+# MAGIC 1. Item 1
+# MAGIC 1. Item 2
+# MAGIC 1. Item 3
+# MAGIC Unordered list:
+# MAGIC - Item a
+# MAGIC - Item b
+# MAGIC - Item c
+
+# COMMAND ----------
+
+
